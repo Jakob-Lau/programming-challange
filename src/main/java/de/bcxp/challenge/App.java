dd51bbd77c5ca75237482bff3bc33268394f867d
Jakob Lauterbach <j.lauterbach@ingburo.de>
2023-02-06 20:11:09 +0100

    Added error handling

--- src/main/java/de/bcxp/challenge/App.java
+++ src/main/java/de/bcxp/challenge/App.java
@@ -3,7 +3,6 @@ package de.bcxp.challenge;
 import de.bcxp.challenge.countries.CountryAnalysis;
 import de.bcxp.challenge.weather.WeatherAnalysis;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,23 +17,34 @@ public final class App {
      * This is the main entry method of your program.
      * @param args The CLI arguments passed
      */
-    public static void main(String... args) throws IOException {
-        // create weather analysis
-        String[] weatherPathArray = {"src", "main", "resources", "de", "bcxp", "challenge", "weather.csv"};
-        final Path weatherPath = Paths.get(String.join(File.separator, weatherPathArray));
-        WeatherAnalysis weatherAnalysis = new WeatherAnalysis(weatherPath, ',');
-
-        // create countries analysis
-        String[] countriesPathArray = {"src", "main", "resources", "de", "bcxp", "challenge", "countries.csv"};
-        final Path countriesPath = Paths.get(String.join(File.separator, countriesPathArray));
-        CountryAnalysis countryAnalysis = new CountryAnalysis(countriesPath, ';');
-
-
+    public static void main(String... args) {
         // analysis calls
-        int dayWithSmallestTempSpread = weatherAnalysis.calcSmallestTempSpread().day;
-        System.out.printf("Day with smallest temperature spread: %d%n", dayWithSmallestTempSpread);
+        String dayWithSmallestTempSpread = getDayWithSmallestTemperatureSpread();
+        System.out.printf("Day with smallest temperature spread: %s%n", dayWithSmallestTempSpread);
 
-        String countryWithHighestPopulationDensity = countryAnalysis.getHighestPopulationDensityCountry().getName();
+        String countryWithHighestPopulationDensity = getHighestPopulationDensityCountry();
         System.out.printf("Country with highest population density: %s%n", countryWithHighestPopulationDensity);
+    }
+
+    private static String getDayWithSmallestTemperatureSpread() {
+        try {
+            final Path weatherPath = Paths.get("src/main/resources/de/bcxp/challenge/weather.csv");
+            // get analysis class
+            WeatherAnalysis weatherAnalysis = new WeatherAnalysis(weatherPath, ',');
+            return String.valueOf(weatherAnalysis.calcSmallestTempSpread().day);
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    private static String getHighestPopulationDensityCountry() {
+        try {
+            final Path countriesPath = Paths.get("src/main/resources/de/bcxp/challenge/countries.csv");
+            // get analysis class
+            CountryAnalysis countryAnalysis = new CountryAnalysis(countriesPath, ';');
+            return  countryAnalysis.getHighestPopulationDensityCountry().getName();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
     }
 }
--- src/main/java/de/bcxp/challenge/csv/CsvReader.java
+++ src/main/java/de/bcxp/challenge/csv/CsvReader.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class CsvReader<T> {
 
     public List<T> readFile(Path path, char separator, Class<T> clazz) throws IOException {
+        checkFile(path);
         try (BufferedReader br = Files.newBufferedReader(path)) {
             CsvToBean<T> beans = new CsvToBeanBuilder<T>(br)
                     .withType(clazz)
@@ -25,5 +26,25 @@ public class CsvReader<T> {
 
             return beans.parse();
         }
+    }
+
+    private void checkFile(Path path) throws IOException {
+        File file = path.toFile();
+        // you could check the file extension first
+
+        // check if file exists
+        if (!file.exists()) {
+            throw new FileNotFoundException("File " + file.getAbsolutePath() + " not found.");
+        }
+
+        // check if file is empty
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line = br.readLine();
+        if (line == null) {
+            // you could use a custom EmptyFileException
+            throw new IllegalArgumentException("File " + file.getAbsolutePath() + " is empty.");
+        }
+
+        // here you could check for the headers that are expected
     }
 }
